2d554b8562845a4672a8e25c65e0daeff0635fe5
daniapc <daniel.castro0811@gmail.com>
2024-11-07 21:02:27 -0300

    getting tracks from playlist and saving in csv

--- src/api.py
+++ src/api.py
@@ -10,37 +10,30 @@ SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
 SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
 SPOTIPY_REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
 
-playlist_url = 'spotify:playlist:5UkNVuZ9dCwnLFWi2rglRM'
+playlist_id = '5UkNVuZ9dCwnLFWi2rglRM'
 charli_url = 'spotify:artist:25uiPmTg16RbhZWAqwLBy5'
+my_user_id = '07kx17nd6nebncli9z3jcvhgj'
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                                client_secret=SPOTIPY_CLIENT_SECRET,
                                                redirect_uri=SPOTIPY_REDIRECT_URI,
                                                scope="user-library-read"))
 
+tracks = []
+playlist_tracks = sp.playlist_tracks(playlist_id=playlist_id)
+items = playlist_tracks['items']
 
+df = pd.DataFrame(columns=('name', 'artist', 'id'))
+i = 0
 
-# results = sp.artist_albums(charli_url, album_type='album')
-# albums = results['items']
-# while results['next']:
-#     results = sp.next(results)
-#     albums.extend(results['items'])
+for item in items:
+    track = item['track']
+    track_name = track['name']
+    track_id = track['id']
+    track_artist = track['artists'][0]['name']
 
-# for album in albums:
-#     print(album['name'])
+    df.loc[i] = list([track_name, track_artist, track_id])
 
+    i += 1
 
-playlists = sp.user_playlists('spotify')
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
-# while results['next']:
-#     results = sp.next(results)
-#     musics.extend(results['items'])
-
-# for album in musics:
-#     print(album['name'])
+df.to_csv(os.getcwd() + '/src/data/tracks.csv', index=False)
